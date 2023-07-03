bc321aaea1539d75476141f3c5ce6dc57726ec3d
Luciano Fernandes <lucianoffernands@gmail.com>
2023-07-02 21:30:53 -0300

    Catalog cards

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import Header from "../../components/Header";
 import SearchBar from '../../components/SearchBar';
+import Card from '../../components/card';
 
 export default function Catalog() {
    
@@ -15,7 +16,16 @@ export default function Catalog() {
             </div>
 
             <div className="ct-catalog-cards-container">
-              <div>Teste</div>
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
             </div>
 
           </section>  
